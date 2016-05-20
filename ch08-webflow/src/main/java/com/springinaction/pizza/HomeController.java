e06d32d09c01ec26c1d319878ef9ec5fca4afc12
wangtao <wangtao@mogoroom.com>
2016-05-20 16:39:17 +0800

    add ch08 webflow

---
+++ ch08-webflow/src/main/java/com/springinaction/pizza/HomeController.java
@@ -0,0 +1,15 @@
+package com.springinaction.pizza;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+	@RequestMapping("/")
+	public String redirectToFlow() {
+		return "redirect:/pizza";
+//		return "redirect:/pizza-singleflow";
+	}
+
+}
