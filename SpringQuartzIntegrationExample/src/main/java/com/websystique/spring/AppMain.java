1b5e0d20fb8f6dc6603cc3ebad56093a882ce3d7
wangtao <wangtao@mogoroom.com>
2016-06-01 10:44:24 +0800

    add

--- SpringQuartzIntegrationExample/src/main/java/com/websystique/spring/AppMain.java
+++ SpringQuartzIntegrationExample/src/main/java/com/websystique/spring/AppMain.java
@@ -4,6 +4,7 @@ import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class AppMain {
+	
 	public static void main(String args[]){
 		AbstractApplicationContext context = new ClassPathXmlApplicationContext("quartz-context.xml");
 	}
