1f6359c825732e3f8937ca92bf70efb0f7033597
wangtao <wangtaoch731@163.com>
2017-06-22 16:39:04 +0800

    something else

--- websocket-messaging/source/production/java/com/wrox/site/AuthenticationEvent.java
+++ websocket-messaging/source/production/java/com/wrox/site/AuthenticationEvent.java
@@ -3,7 +3,9 @@ package com.wrox.site;
 import java.io.Serializable;
 
 public abstract class AuthenticationEvent extends ClusterEvent {
-	
+
+	private static final long serialVersionUID = 1L;
+
 	public AuthenticationEvent(Serializable source) {
 		super(source);
 	}
